d4775f06a3440ba974378296515004219169b047
Poonam <115942504+IP0527@users.noreply.github.com>
2025-08-08 08:12:02 +0530

    Reverse_a_number.cpp

--- Reverse_a_number.cpp
+++ Reverse_a_number.cpp
@@ -1,31 +1,33 @@
+// Reverse of a number 
 
-    #include<iostream>
+    #include<iostream>    // include input/output stream
     using namespace std;
 
     int main()
     {
 
-        int number ;
-        int original_number = number;
+        int number ;       // take input variable
         int last_digit;
-        int rev = 0;
-    
-        cout << " Enter a number : ";
-        cin >> number;
+        int rev = 0;      // take input to store the reverse number 
+        
+        
+        cout << " Enter a number : ";   // take input numbr from user    
+        cin >> number;           // Read input from user 
+        int original_number = number;
         
         
         do
         {
-            last_digit = number % 10;
-            rev = rev * 10 + last_digit;
-            number /= 10;
+            
+            last_digit = number % 10;        // get last digit of the number 
+            rev = rev * 10 + last_digit;     // Expression for store the last digit
+            number /= 10;                 // Remove last digit 
 
         }
         while(number != 0);
 
-        cout << " Reverse of is " << rev;
+        cout << " Reverse of " << original_number << " is " << rev;
 
-        return 0;
+        return 0; // End of Program
 
     }
-    
